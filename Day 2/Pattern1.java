225e114e38e3009edbb161d9e4f0d04b8d4133f3
unknown <anuragmishra11904@gmail.com>
2025-04-15 18:24:19 +0530

    simple trpangle pattern

--- Day 2/Pattern1.java
+++ Day 2/Pattern1.java
@@ -1,6 +1,12 @@
 public class Pattern1
-{
+{//code of simple triangle by stars
     public static void main(String[] args) {
-        System.out.println("pattern");
+        for(int i=0;i<=5;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
     }
 }
