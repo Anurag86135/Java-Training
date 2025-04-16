81a7dc2e4e9edaf4a920492ba79cf0cdee77d4f8
unknown <anuragmishra11904@gmail.com>
2025-04-16 19:27:41 +0530

    Factorial with recursive function

---
+++ day3/Factorial.java
@@ -0,0 +1,18 @@
+package day3;
+
+public class Factorial {
+    public static int factorialRecursive(int n){
+    if((n==0)||(n==1)){
+        return 1;
+}else{
+       return  n*factorialRecursive(n-1);
+    }
+    
+}
+
+public static void main(String[] args){
+    int number =5;
+    int result = factorialRecursive(number);
+    System.out.println("Factorial of "+ number +" is :"+result);
+}
+}
