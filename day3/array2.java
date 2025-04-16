98013a455c71df463e6e7d261a0728a8e2ed7374
unknown <anuragmishra11904@gmail.com>
2025-04-16 18:58:40 +0530

    Taking array and its size from user as an input

---
+++ day3/array2.java
@@ -0,0 +1,23 @@
+package day3;
+
+import java.util.Scanner;
+
+public class array2 {
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the size of the array");
+        int n =sc.nextInt();
+        int arr[] =new int[n];
+        int sum =0;
+
+        System.out.println("enter the elements");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+            sum+=arr[i];
+        }
+        System.out.println("sum = "+ sum);
+    }
+    
+    
+}
