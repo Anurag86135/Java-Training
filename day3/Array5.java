bf6d3af12c9d42c38fc31f0667c83d7ee8fbe596
unknown <anuragmishra11904@gmail.com>
2025-04-17 15:48:16 +0530

    count of odd and even in array

--- day3/Array5.java
+++ day3/Array5.java
@@ -1,11 +1,42 @@
-package day3;
-
 import java.util.Scanner;
 public class Array5 {
+    //Array ke ander kitne odd or even numbers hain , uska count nikalo;
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
-        System.out.println("enter the size of the array:")
+        System.out.println("enter the size of the array:");
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        int even = 0, odd =0;
+
+        System.out.println("enter the elements");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+
+            //for showing array
+            
+            for(int i=0;i<n;i++){
+               
+               // System.out.println("the array is given as according to you4r inputs " + arr[i]);
+            if (arr[i]%2==0){
+           //System.out.println(arr[i] +":"+ "even");// even++;
+           even++;
+            }
+            else{
+            //odd++;sout    
+           // System.out.println(arr[i] + ":" + "odd");
+           odd++;
+            }
+            //System.out.println("Even "+ even + " , odd = "+ odd);
+
+
+        }
+        System.out.println("Even "+ even + " , odd = "+ odd);
+4
+1
+
         
-    }
+    
     
 }
+    }
