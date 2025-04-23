4c3ed63016cef08b3a52ce043bb4741b37d5064f
unknown <anuragmishra11904@gmail.com>
2025-04-23 10:50:11 +0530

    delete dulpicate elements in the string

---
+++ day3/String2.java
@@ -0,0 +1,22 @@
+package day3;
+
+public class String2 {
+    public static void main(String[] args) {
+        String str ="helllo";
+        String newstr ="";
+        for(int i=0;i<str.length();i++){
+            char c =str.charAt(i);
+
+            if(newstr.indexOf(c)==-1){
+                newstr+=c;
+            }
+            
+
+
+        }
+         System.out.println(newstr);
+
+    }
+    //System.out.println(newstr);
+    
+}
