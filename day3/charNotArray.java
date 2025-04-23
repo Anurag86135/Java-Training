36af25533aaabdf188641dca3e91c0c2434459bf
unknown <anuragmishra11904@gmail.com>
2025-04-23 20:54:20 +0530

    finding first duplicate character in the given string without using charater array

--- day3/charNotArray.java
+++ day3/charNotArray.java
@@ -1,19 +1,16 @@
 package day3;
 
-public class charArray {
+public class charNotArray {
     public static void main(String[] args) {
-        String str="helooo";
-        // char[] ch=str.charAt();
-        //{'h','e','l','l',''o'}
+        String str="Anuragg";
         for(int i=0;i<str.length();i++){
             for(int j=i+1;j<str.length();j++){
                 if(str.charAt(i)==str.charAt(j)){
-                    System.out.println(str.charAt(i));
+                    System.out.println("the duplicate character in the given "+str+ " is  "+ "'"+str.charAt(j)+"'");
                     return;
                 }
             }
         }
-
     }
     
 }
