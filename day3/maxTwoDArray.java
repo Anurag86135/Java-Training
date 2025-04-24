84188ffbdc9f510c08de3b1701cc0ef8e9011495
unknown <anuragmishra11904@gmail.com>
2025-04-24 19:58:03 +0530

    finding maximum element in the two dimentioal array

--- day3/maxTwoDArray.java
+++ day3/maxTwoDArray.java
@@ -2,6 +2,21 @@ package day3;
 
 public class maxTwoDArray {
     public static void main(String[] args){
+        int array[][] ={{1,2,3},
+                        {4,5,6},
+                        {7,8,9}
+
+        };
+        int max=array[0][0];
+        for(int i=0;i<array.length;i++){
+            for(int j=0;j<array[i].length;j++){
+                if(array[i][j]>max){
+                    max=array[i][j];
+
+                }
+            }
+        }
+        System.out.println("the maximum element of the  given array is "  +max);
 
     }
     
