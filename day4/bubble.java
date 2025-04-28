facde289322eaf5a5382200fab5ccd3a96ce2619
unknown <anuragmishra11904@gmail.com>
2025-04-28 15:36:09 +0530

    bubble short

--- day4/bubble.java
+++ day4/bubble.java
@@ -1,23 +1,35 @@
 package day4;
 
 public class bubble {
-    int temp=0;
-    static void BubbleShort(int [] arr){
+    //int temp=0;
+    static void BubbleShort(int[] arr){
         for(int i=0;i<arr.length-1;i++){
             for(int j=0;j<arr.length-i-1;j++){
                 if(arr[j]>arr[j+1]){
                     
-                    temp=arr[j];
+                   int  temp =arr[j];
                     arr[j+1]=arr[j];
                     arr[j+1]=temp;
 
                     
-                }
+                
             }
+        
+        
+
         }
+   
+
+    }
         for(int i:arr){
-            
+            System.out.println(i);
+
         }
+    }
+    public static void main(String[] args) {
+        int[] arr={1,2,3,6,5,9};
+        BubbleShort(arr);
+      
     }
     
 }
