149c3a7ba16c3857b78ddde1e769ba4b06ef0e61
Anurag <anuragmishra11904@gmail.com>
2025-05-08 13:59:23 +0530

    using package in same folder in different file

--- day8/main.java
+++ day8/main.java
@@ -1,3 +1,17 @@
+// package day8;//transfer the data of the file from one file to another through pakages.
+
+//  public class file {
+//     public void Anurag() {
+//         String str = "Anurag is always with you all";
+//         System.out.println(str);
+
+//     }
+    
+// }
+
+
+
+
 package day8;//transfer the data of one file  to anothe rone through packsges
 
 public class main {
