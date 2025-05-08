fe8d55e41bcca786e64f5ceed8ed9c80d244193f
Anurag <anuragmishra11904@gmail.com>
2025-05-08 12:40:51 +0530

    making objects

---
+++ day9/obj.java
@@ -0,0 +1,27 @@
+package day9;
+
+public class obj {
+   
+        public void sum(){
+            int a=22;
+            int b=22;
+            int sum =0;
+            sum=(a+b);
+            System.out.println(sum);
+
+
+        }
+       
+
+}
+    
+    
+ class main{
+
+    public static void main (String[] args) {
+        obj b=new obj();
+        b.sum();
+    }
+}
+
+
