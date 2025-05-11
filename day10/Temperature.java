a2fb9cc8ab91b32999923375cde584156aa111e7
Anurag <anuragmishra11904@gmail.com>
2025-05-11 11:45:14 +0530

    Create a class Rectangle with private fields length and width

--- day10/Temperature.java
+++ day10/Temperature.java
@@ -1,27 +1,28 @@
-package day10;
+// package day10;
 
-//import org.w3c.dom.Text;
+// //import org.w3c.dom.Text;
 
-// 4. Write a class `Temperature` that has a private variable `celsius`. Provide methods to convert and
-//  return the temperature in Fahrenheit using getter
-class Temp {
-    private float celsius ;
-    void getData(float celsius){
-    this.celsius=celsius;
-    }
-float convert(){//
-    float fahrenheit=(celsius*9/5)+32;
-    System.out.println("fahrenheit in respect to celsius "+celsius +" is " + fahrenheit);
-    return fahrenheit;
+// // 4. Write a class `Temperature` that has a private variable `celsius`. Provide methods to convert and
+// //  return the temperature in Fahrenheit using getter
+// class Temp {
+//     private float celsius ;
+//     void getData(float celsius){
+//     this.celsius=celsius;
+//     }
+// float convert(){//
+//     float fahrenheit=(celsius*9/5)+32;
+//     System.out.println("fahrenheit in respect to celsius "+celsius +" is " + fahrenheit);
+//     return fahrenheit;
 
     
-}
+// }
     
-}
-public class Temperature{
-    public static void main(String[] args) {
-        Temp obj=new Temp();
-        obj.getData(32);
-        obj.convert();
-    }
-}
+// }
+// public class Temperature{
+//     public static void main(String[] args) {
+//         Temp obj=new Temp();
+//         obj.getData(32);
+//         obj.convert();
+//     }
+// }
+
